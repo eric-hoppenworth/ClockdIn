520939b3f831c45f2ce56a17d92856a8c34b1464
Daniel <drykan@gmail.com>
2017-08-22 21:16:27 -0400

    Splash page is complete (minus responsive)

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -6,4 +6,19 @@ $.get("/dashboard").then(function(data){
 
 $(document).ready(function() {
     $('#modal1').modal();
+    $('#modal2').modal();
+});
+
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#current-time-display')
+    update();
+    setInterval(update, 1000);
 });
